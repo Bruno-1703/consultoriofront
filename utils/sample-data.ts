d620c86e17bff25de6da45087e4b946dbdcc5aa8
unknown <bruno170396@gmail.com>
2025-12-03 19:44:13 -0300

    fix build

---
+++ utils/sample-data.ts
@@ -0,0 +1,14 @@
+import { User } from "../interfaces";
+
+export const sampleUserData: User[] = [
+  {
+    id: 1,
+    name: "Juan Perez",
+    // Add other User fields as needed
+  },
+  {
+    id: 2,
+    name: "Maria Gomez",
+    // Add other User fields as needed
+  },
+];
